e274f8eff93ccdedf3f1690c102011366213bb45
xiejunxin <1833610970@qq.com>
2023-04-25 00:29:28 +0800

    初步对kout增加了十六进制输出的功能

--- os/kernel/boot/main.cpp
+++ os/kernel/boot/main.cpp
@@ -7,19 +7,40 @@
 
 int main()
 {
-    kout<<"hello world"<<"\n";
-    kout[red]<<"clock_init"<<endl;
-    clock_init();
-    kout[red]<<"trap_init"<<endl;
-    trap_init();
-    kout[red]<<"interrupt_enable"<<endl;
-    interrupt_enable(); 
+    kout << "hello world!" << "\n";
 
-    
+    /*
+    //test outHex
+    uint64 xu64 = 123456789987654321;
+    uint32 xu32 = 123456789;
+    uint16 xu16 = 12345;
+    uint8 xu8 = 123;
+    int64 x64 = -12345678987654321;
+    int32 x32 = -123456789;
+    int16 x16 = -12345;
+    int8 x8 = -123;
+    kout.hex(xu64) << endl;
+    kout.hex(xu32) << endl;
+    kout.hex(xu16) << endl;
+    kout.hex(xu8) << endl;
+    kout.hex(x64) << endl;
+    kout.hex(x32) << endl;
+    kout.hex(x16) << endl;
+    kout.hex(x8) << endl;
+    */
+
+    kout[red] << "clock_init" << endl;
+    clock_init();
+    kout[red] << "trap_init" << endl;
+    trap_init();
+    kout[red] << "interrupt_enable" << endl;
+    interrupt_enable();
+
     while (1)
     {
         delay(100);
-        kout<<'*';
+        kout << '*';
     }
+    
     return 0;
 }
--- os/kernel/lib/kout.cpp
+++ os/kernel/lib/kout.cpp
@@ -2,103 +2,198 @@
 
 KOUT kout;
 
-void puts(const char * str)
+void puts(const char* str)
 {
     while (*str)
     {
         sbi_putchar(*str);
         str++;
     }
-    
+
 }
 
-KOUT & KOUT::operator<<(const char * str)
+KOUT& KOUT::operator<<(const char* str)
 {
     puts(str);
-    return * this;
+    return *this;
 }
 
-KOUT & KOUT::operator<<(uint64 num)
+KOUT& KOUT::operator<<(uint64 num)
 {
     char buffer[21];
-    int i=0;
-    if (num==0)
+    int i = 0;
+    if (num == 0)
     {
         sbi_putchar('0');
         return *this;
     }
-    
+
     while (num)
     {
-        buffer[i++]=num%10;
-        num/=10;
+        buffer[i++] = num % 10;
+        num /= 10;
     }
     i--;
-    while (i>=0)
+    while (i >= 0)
     {
-        sbi_putchar('0'+buffer[i--]);
+        sbi_putchar('0' + buffer[i--]);
     }
-    
-    return *this; 
-    
+
+    return *this;
+
 }
-KOUT & KOUT::operator<<(uint32 num)
+
+KOUT& KOUT::operator<<(uint32 num)
 {
-    return *this<<(uint64)num;
+    return *this << (uint64)num;
 }
-KOUT & KOUT::operator<<(uint16 num)
+
+KOUT& KOUT::operator<<(uint16 num)
 {
-    return *this<<(uint64)num;
+    return *this << (uint64)num;
 }
-KOUT & KOUT::operator<<(uint8 num)
+
+KOUT& KOUT::operator<<(uint8 num)
 {
-    return *this<<(uint64)num;
+    return *this << (uint64)num;
 }
-KOUT & KOUT::operator<<(int64 num)
+
+KOUT& KOUT::operator<<(int64 num)
 {
-    if (num<0)
+    if (num < 0)
     {
         sbi_putchar('-');
-        num=-num;
+        num = -num;
     }
     return *this << (uint64)num;
 }
-KOUT & KOUT::operator<<(int32 num)
+
+KOUT& KOUT::operator<<(int32 num)
 {
     return *this << (int64)num;
 }
-KOUT & KOUT::operator<<(int16 num)
+
+KOUT& KOUT::operator<<(int16 num)
 {
     return *this << (int64)num;
 }
-KOUT & KOUT::operator<<(int8 num)
+
+KOUT& KOUT::operator<<(int8 num)
 {
     sbi_putchar(num);
     return *this;
 }
 
-
-KOUT & KOUT::operator[](COLOR _color)
+KOUT& KOUT::operator[](COLOR _color)
 {
-    *this<<"\e["<<(uint8)_color<<'m';
+    *this << "\e[" << (uint8)_color << 'm';
 
     return *this;
 }
 
-KOUT& endl(KOUT & kout)
+KOUT& KOUT::hex(uint64 data, bool is_large)
 {
-    kout<<'\n';
-    kout<<"\e[0m" ;
+    *this << "0x";
+    int operator_cnt = 8;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(uint32 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 4;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(uint16 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 2;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(uint8 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 1;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(int64 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 8;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(int32 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 4;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(int16 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 2;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& KOUT::hex(int8 data, bool is_large)
+{
+    *this << "0x";
+    int operator_cnt = 1;
+    while (operator_cnt--) {
+        int shiftdigits = operator_cnt * 8;
+        outHex((unsigned char)(data >> shiftdigits), is_large);
+    }
+    return *this;
+}
+
+KOUT& endl(KOUT& kout)
+{
+    kout << '\n';
+    kout << "\e[0m";
     return kout;
 }
-KOUT& clean(KOUT & kout)
+
+KOUT& clean(KOUT& kout)
 {
-    kout<<"\e[0m";
+    kout << "\e[0m";
     return kout;
 }
 
 
-KOUT & KOUT::operator<<(KOUT & (*p)(KOUT & kout))
+KOUT& KOUT::operator<<(KOUT& (*p)(KOUT& kout))
 {
     return p(*this);
 }
